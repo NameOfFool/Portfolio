c3c0e8d953de0b5da207f57613f59e9f67bd5b9d
Tikhin <tikhonov@qnium.com>
2025-03-04 20:52:55 +0300

    added adaptive

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,17 +1,35 @@
 import { NavLink } from 'react-router-dom'
-import styles from './Navbar.module.scss'   
-import { GoPerson, GoHome } from 'react-icons/go';
+import styles from './Navbar.module.scss'
+import { GoPerson, GoHome, GoChecklist, GoProject } from 'react-icons/go';
+import { BiMenuAltRight } from 'react-icons/bi';
+import { AiOutlineCloseSquare } from 'react-icons/ai';
+import { useState } from 'react';
 
 export default function Navbar() {
+    const [menuOpen, setMenuOpen] = useState(false)
+    const menuToggle = () => setMenuOpen((p) => !p)
     return (
         <nav className={styles.navbar}>
-            <NavLink to="/" className={styles.title}>Portfolio</NavLink>            
-                <ul className={styles.menu}>
-                    <li><NavLink className={({isActive}) =>  isActive ? styles.active:""} to="/"><GoHome className={styles.navIcon}/> Home</NavLink></li>
-                    <li><NavLink className={({isActive}) =>  isActive ? styles.active:""} to="/about"><GoPerson className={styles.navIcon}/> About</NavLink></li>
-                    <li><NavLink className={({isActive}) =>  isActive ? styles.active:""} to="/projects">Projets</NavLink></li>
-                    <li><NavLink className={({isActive}) =>  isActive ? styles.active:""} to="/contacts">Contacts</NavLink></li>
-                </ul>
+            <div className={styles.navbar__content}>
+                <NavLink to="/" className={styles.title}>Portfolio</NavLink>
+                <div className={`${styles.menu} ${menuOpen ? styles.menu__open : ''}`}>
+                    <NavLink className={({ isActive }) => isActive ? styles.active + " " + styles.menu__item : styles.menu__item} to="/">
+                        <GoHome className={styles.navIcon} /> Home
+                    </NavLink>
+                    <NavLink className={({ isActive }) => isActive ? styles.active + " " + styles.menu__item : styles.menu__item} to="/about">
+                        <GoPerson className={styles.navIcon} /> About
+                    </NavLink>
+                    <NavLink className={({ isActive }) => isActive ? styles.active + " " + styles.menu__item : styles.menu__item} to="/projects">
+                        <GoProject className={styles.navIcon} /> Projets
+                    </NavLink>
+                    <NavLink className={({ isActive }) => isActive ? styles.active + " " + styles.menu__item : styles.menu__item} to="/contacts">
+                        <GoChecklist className={styles.navIcon} /> Contacts
+                    </NavLink>
+                </div>
+                <button className={styles.menu__toggler} onClick={menuToggle}>
+                    {!menuOpen ? <BiMenuAltRight /> : <AiOutlineCloseSquare />}
+                </button>
+            </div>
         </nav>
     )
 }
