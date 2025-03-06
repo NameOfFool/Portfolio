bac5d6f91e4e6272bc9633f8306329dcee0597a8
NameOfFool <ivan.pauk2008@gmail.com>
2025-03-06 16:07:21 +0300

    footer added

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,6 +1,7 @@
 import { Outlet, } from 'react-router-dom'
 import styles from './App.module.scss'
 import Navbar from './components/Navbar/Navbar'
+import Footer from './components/Footer/Footer'
 
 
 function Layout() {
@@ -9,6 +10,7 @@ function Layout() {
     <div className={styles.App}>
       <Navbar />
       <Outlet />
+      <Footer />
     </div>
   )
 }
