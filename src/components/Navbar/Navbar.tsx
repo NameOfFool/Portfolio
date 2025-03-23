b9ba21bb80055cbf8a48751a4416d1f387698f91
NameOfFool <ivan.pauk2008@gmail.com>
2025-03-23 14:50:59 +0300

    mobile style fixed

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -5,7 +5,7 @@ import { BiMenuAltRight } from 'react-icons/bi';
 import { AiOutlineCloseSquare } from 'react-icons/ai';
 import { useState } from 'react';
 import logo from '../../assets/logo.png'
-import {  GithubOriginalIcon } from '@devicon/react';
+import { GithubOriginalIcon } from '@devicon/react';
 import LanguageSwitcher from '../LanguageSwitcher/LanguageSwitcher';
 import { useTranslation } from 'react-i18next';
 
@@ -13,29 +13,29 @@ import { useTranslation } from 'react-i18next';
 export default function Navbar() {
     const [menuOpen, setMenuOpen] = useState(false)
     const menuToggle = () => setMenuOpen((p) => !p)
-    const {t} = useTranslation()
-
+    const { t } = useTranslation()
     return (
         <nav className={styles.navbar}>
             <div className={styles.navbar__content}>
-                <NavLink to="/" className={styles.title}><img src={logo}/></NavLink>
-                <div className={`${styles.menu} ${menuOpen ? styles.menu__open : ''}`}>
-                    <NavLink className={({ isActive }) => isActive ? styles.active + " " + styles.menu__item : styles.menu__item} to="/" onClick={() => setMenuOpen(false)}>
-                        <GoHome className={styles.navIcon} /> {t("navbar.home")}
-                    </NavLink>
-                    <NavLink className={({ isActive }) => isActive ? styles.active + " " + styles.menu__item : styles.menu__item} to="/about" onClick={() => setMenuOpen(false)}>
-                        <GoPerson className={styles.navIcon} /> {t("navbar.about")}
-                    </NavLink>
-                    <NavLink className={({ isActive }) => isActive ? styles.active + " " + styles.menu__item : styles.menu__item} to="/projects" onClick={() => setMenuOpen(false)}>
-                        <GoProject className={styles.navIcon} /> {t("navbar.projects")}
-                    </NavLink>
-                    <a href='https://github.com/NameOfFool' target='_blank'><GithubOriginalIcon color='white' size={25} /></a>
-                    <LanguageSwitcher/>
+                <NavLink to="/" className={styles.title}><img src={logo} /></NavLink>
+                <div className={styles.menu__container}>
+                    <div className={`${styles.menu} ${menuOpen ? styles.menu__open : ''}`}>
+                        <NavLink className={({ isActive }) => isActive ? styles.active + " " + styles.menu__item : styles.menu__item} to="/" onClick={() => setMenuOpen(false)}>
+                            <GoHome className={styles.navIcon} /> {t("navbar.home")}
+                        </NavLink>
+                        <NavLink className={({ isActive }) => isActive ? styles.active + " " + styles.menu__item : styles.menu__item} to="/about" onClick={() => setMenuOpen(false)}>
+                            <GoPerson className={styles.navIcon} /> {t("navbar.about")}
+                        </NavLink>
+                        <NavLink className={({ isActive }) => isActive ? styles.active + " " + styles.menu__item : styles.menu__item} to="/projects" onClick={() => setMenuOpen(false)}>
+                            <GoProject className={styles.navIcon} /> {t("navbar.projects")}
+                        </NavLink>
+                        <a href='https://github.com/NameOfFool' target='_blank'><GithubOriginalIcon color='white' size={25} /></a>
+                    </div>
+                    <button className={styles.menu__toggler} onClick={menuToggle}>
+                        {!menuOpen ? <BiMenuAltRight /> : <AiOutlineCloseSquare />}
+                    </button>
+                    <LanguageSwitcher />
                 </div>
-                <button className={styles.menu__toggler} onClick={menuToggle}>
-                    {!menuOpen ? <BiMenuAltRight /> : <AiOutlineCloseSquare />}
-                </button>
-               
             </div>
         </nav>
     )
